68129b44f4c6697ce200da44048cc51fca6ea3e4
tomagrab <tommygrabowski@MacBook-Pro-11262.local>
2023-08-22 12:41:43 -0400

    Updated UI

--- src/Main/Database/db.ts
+++ src/Main/Database/db.ts
@@ -3,15 +3,7 @@ import { PrismaClient } from "@prisma/client";
 export const db = new PrismaClient();
 
 async function main() {
-  const note = await db.note.create({
-    data: {
-      title: "My first note",
-      actions: "This is my first action",
-      content: "This is my first note",
-      summary: "This is my first summary",
-    },
-  });
-  console.log(note);
+  console.log("Database connected");
 }
 
 main()
